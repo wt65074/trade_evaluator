a836f9e826f7fdde6feccc3ae2b996cd68688cd7
William Tobey <williamtobey@Williams-MacBook-Air.local>
2024-05-26 17:56:57 -0700

    Add a belongs_to relationship to trades

--- app/models/trade.rb
+++ app/models/trade.rb
@@ -2,6 +2,8 @@ class Trade < ApplicationRecord
   serialize :team_a_picks, coder: JSON, type: Array
   serialize :team_b_picks, coder: JSON, type: Array
   validates :team_a, :team_b, presence: true
+  belongs_to :team_a, class_name: 'Team', foreign_key: 'team_a', primary_key: 'shortname'
+  belongs_to :team_b, class_name: 'Team', foreign_key: 'team_b', primary_key: 'shortname'
 
   def all_model_scores
     all_models = ValueModel.all_models_cached
