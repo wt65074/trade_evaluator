17b70915e5f28f25c875e98c727a6c6f981a1382
William Tobey <williamtobey@Williams-MacBook-Air.local>
2024-05-26 18:00:30 -0700

    Fix colors

--- app/javascript/controllers/results_controller.js
+++ app/javascript/controllers/results_controller.js
@@ -26,17 +26,18 @@ export default class extends Controller {
       const valueA = teamAPicks.map(pick => Number(model.values[pick])).reduce((acc, val) => acc + val, 0)
       const valueB = teamBPicks.map(pick => Number(model.values[pick])).reduce((acc, val) => acc + val, 0)
 
+      console.log(valueA, valueB)
+
       return [id,
         Math.round(100 * (valueB - valueA)) / 100
       ]
     }))
-    console.log(modelScores)
 
     this.teamAScoreTargets.forEach((elem, _) => {
       const modelId = Number(elem.dataset.modelId);
       const modelScore = modelScores.get(modelId);
       elem.textContent = `${modelScore > 0 ? '+' : ''}${modelScore}`
-      if (modelScore < 0) {
+      if (modelScore > 0) {
         elem.classList.add("bg-green-100")
       } else {
         elem.classList.remove("bg-green-100")
@@ -47,7 +48,7 @@ export default class extends Controller {
       const modelId = Number(elem.dataset.modelId);
       const modelScore = -modelScores.get(modelId);
       elem.textContent = `${modelScore > 0 ? '+' : ''}${modelScore}`
-      if (modelScore < 0) {
+      if (modelScore > 0) {
         elem.classList.add("bg-green-100")
       } else {
         elem.classList.remove("bg-green-100")
